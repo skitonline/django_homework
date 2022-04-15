fc42a186297381243b3903d68240afe2b2edc1ae
Alexander Evseev <skitonline@yandex.ru>
2022-04-15 15:03:46 +0400

    ab test done

--- request-handling/landing/app/views.py
+++ request-handling/landing/app/views.py
@@ -12,6 +12,8 @@ counter_click = Counter()
 
 def index(request):
     # Реализуйте логику подсчета количества переходов с лендига по GET параметру from-landing
+    from_landing = request.GET.get('from-landing')
+    counter_click[from_landing] += 1
     return render(request, 'index.html')
 
 
@@ -20,13 +22,22 @@ def landing(request):
     # в зависимости от GET параметра ab-test-arg
     # который может принимать значения original и test
     # Так же реализуйте логику подсчета количества показов
-    return render(request, 'landing.html')
+
+    ab_test_arg = request.GET.get('ab-test-arg')
+    counter_show[ab_test_arg] += 1
+
+    if ab_test_arg == 'original':
+        show_page = 'landing.html'
+    else:
+        show_page = 'landing_alternate.html'
+
+    return render(request, show_page)
 
 
 def stats(request):
     # Реализуйте логику подсчета отношения количества переходов к количеству показов страницы
     # Для вывода результат передайте в следующем формате:
     return render(request, 'stats.html', context={
-        'test_conversion': 0.5,
-        'original_conversion': 0.4,
+        'test_conversion': 'нет кликов' if counter_click['test'] == 0 else counter_click['test']/counter_show['test'],
+        'original_conversion': 'нет кликов' if counter_click['original'] == 0 else counter_click['original']/counter_show['original'],
     })
