258d8859719915700ca801b90e4d0086227b667d
Alexander Evseev <skitonline@yandex.ru>
2022-04-20 10:01:14 +0400

    bus station done

--- request-handling/pagination/app/views.py
+++ request-handling/pagination/app/views.py
@@ -1,5 +1,9 @@
+from django.core.paginator import Paginator
 from django.shortcuts import render, redirect
 from django.urls import reverse
+from django.conf import settings
+
+import csv
 
 
 def index(request):
@@ -7,13 +11,36 @@ def index(request):
 
 
 def bus_stations(request):
-    current_page = 1
-    next_page_url = 'write your url'
+    data = []
+    with open(settings.BUS_STATION_CSV, newline='') as csvfile:
+        reader = csv.DictReader(csvfile)
+        for row in reader:
+            data.append({'Name': row['Name'],
+                         'Street': row['Street'],
+                         'District': row['District']})
+
+    get_param = 'page'
+    current_page = int(request.GET.get(get_param, 1))
+    elements_per_page = 10
+    paginator = Paginator(data, elements_per_page)
+    page = paginator.page(current_page)
+
+    if page.has_previous():
+        prev_page_url = reverse('bus_stations')
+        prev_page_url += f'?{get_param}={current_page - 1}'
+    else:
+        prev_page_url = None
+
+    if page.has_next():
+        next_page_url = reverse('bus_stations')
+        next_page_url += f'?{get_param}={current_page + 1}'
+    else:
+        next_page_url = None
+
     return render(request, 'index.html', context={
-        'bus_stations': [{'Name': 'название', 'Street': 'улица', 'District': 'район'},
-                         {'Name': 'другое название', 'Street': 'другая улица', 'District': 'другой район'}],
+        'bus_stations': page.object_list,
         'current_page': current_page,
-        'prev_page_url': None,
+        'prev_page_url': prev_page_url,
         'next_page_url': next_page_url,
     })
 
